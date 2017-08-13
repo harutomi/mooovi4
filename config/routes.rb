aec0f7aa6c5e3aa195ba12fc969667263996d095
富永治明 <tominagaharuaki@MacBook-Pro.local>
2017-08-13 18:24:32 +0900

    step3-5 complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 TechReviewSite::Application.routes.draw do
     root 'products#index'
     get 'products' => 'products#index'
+    get 'products/search' => 'products#search'
     get 'products/:id' => 'products#show'
 end
