4a15896b5fca0d4ba941ca2c3e462103d92942c9
富永治明 <tominagaharuaki@MacBook-Pro.local>
2017-08-14 17:31:07 +0900

    step3 mission complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 TechReviewSite::Application.routes.draw do
-    root 'products#index'
-    get 'products' => 'products#index'
-    get 'products/search' => 'products#search'
-    get 'products/:id' => 'products#show'
+  resources :products, only: :show do
+    resources :reviews, only: [:new, :create]
+    collection do
+      get 'search'
+    end
+  end
+  root 'products#index'
+
 end
